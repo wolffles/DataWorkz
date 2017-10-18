c9bc78ce0c42dcda93e43d8ec2780efecdb046de
woffles <wolfgang.truong@gmail.com>
2017-10-18 12:56:22 -0700

    removed more files that are unused, and model specs

--- spec/models/registered_application_spec.rb
+++ spec/models/registered_application_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe RegisteredApplication, type: :model do
+  let(:my_user) { FactoryGirl.create(:user) }
 
+  let(:my_app) { RegisteredApplication.create!(name: "name", url: "url.com", user: my_user) }
+
+  describe "attributes" do
+    it "has has a name, and url" do
+      expect(my_app).to have_attributes(name: "name", url: "url.com" )
+    end
+  end
 end
