855cc04d83b5f53d25a0e23004b93a9351d41287
woffles <wolfgang.truong@gmail.com>
2017-10-22 22:57:50 -0700

    created a way for dataworkz to recieve incoming events, to registered applications. added inflections, and created routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   #  :lockable, :timeoutable and :omniauthable
 
-  has_many :registered_application, dependent: :destroy
+  has_many :registered_applications, dependent: :destroy
   enum role:[:standard, :admin]
   after_initialize {self.role ||= :standard}
 
