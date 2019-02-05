cffbe2d94509b637c0b849eb2cc55790e6e2f09e
woffles <wolfgang.truong@gmail.com>
2019-02-05 11:01:32 -0800

    perfecting demo login

--- app/assets/javascripts/input.js
+++ app/assets/javascripts/input.js
@@ -1,7 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var email_input = document.querySelector('input[id="user_email"]')
-  email_input.value = "wolfgang.truong@gmail.com"
-  
-  var password_input = document.querySelector('input[id="user_password"]');
-  password_input.value = "password"
-})
+window.addEventListener("turbolinks:load", function() {
+  if (document.querySelector('input[id="user_email"]')){
+    var email_input = document.querySelector('input[id="user_email"]');
+    email_input.value = "wolfgang.truong@gmail.com";
+  }
+  if (document.querySelector('input[id="user_password"]')){
+    var password_input = document.querySelector('input[id="user_password"]');
+    password_input.value = "password";
+  }
+});
