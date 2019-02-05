e39db4140267d3fa2d6012617ecd951b59050f54
woffles <wolfgang.truong@gmail.com>
2019-02-05 10:47:38 -0800

    demo login

---
+++ app/assets/javascripts/input.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var email_input = document.querySelector('input[id="user_email"]')
+  email_input.value = "wolfgang.truong@gmail.com"
+  
+  var password_input = document.querySelector('input[id="user_password"]');
+  password_input.value = "password"
+})
